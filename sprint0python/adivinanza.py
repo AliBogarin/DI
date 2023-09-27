cea7730df4c5a511a8415b17b101a2d80338b0ee
Alicia Bogarin <abogarinv@fpcoruna.afundacion.org>
2023-09-27 09:23:28 +0200

    Ejercicio 1, Adivinanza.

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,14 @@
+def adivinanza():
+    print("¿Qué es algo que puedes atrapar, pero no lanzar?")
+    print("a) Un resfriado")
+    print("b) Un balón")
+    print("c) Un libro")
+
+    respuesta = input("Por favor, responde con a, b o c para indicar tu elección: ")
+
+    if respuesta.lower() == 'a':
+        print("¡Correcto! La respuesta es un resfriado.")
+    else:
+        print("Lo siento, esa no es la respuesta correcta. La respuesta correcta es 'a', un resfriado.")
+
+adivinanza()
