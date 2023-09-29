000f979e3bed8a7e31a5f21b9c75407dfdb2dcef
AliBogarin <145338862+AliBogarin@users.noreply.github.com>
2023-09-29 13:27:51 +0200

    Add files via upload

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,15 +1,14 @@
 def suma (num1, num2):
-    print("La suma es: "+int(num1+num2))
     return num1+num2
 
 def resta(num1, num2):
     if num1<num2:
-        return print(" La resta es: ", num1-num2)
+        return  num1-num2
     else:
         return num2-num1
 
 def multiplicacion(num1, num2):
-    return print("La multiplicación es: ", num1*num2)
+    return  num1*num2
 
 def division(num1,num2):
     if num1==0 | num2==0:
@@ -19,9 +18,3 @@ def division(num1,num2):
     else:
         return print("La división es: ", num1-num2)
     
-respuesta= input("Qué operación desea realizar (suma 's'/resta 'r'/ multiplicación 'm'/ división 'd'): ")
-
-if respuesta=="s":
-    num1= input(" 1º número: ")
-    num2= input(" 2º número: ")
-    suma(num1,num2)
