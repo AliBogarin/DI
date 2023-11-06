43eeeaebe69609b3b4a4c3fc9cf5d0b7d227c755
Alicia Bogarin <abogarinv@fpcoruna.afundacion.org>
2023-11-06 17:29:06 +0100

    Ejercicio #2, Sprint #3, Creación de button, Completado.

--- sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,4 +1,19 @@
 package com.example.mycatalog;
 
-public class CatalogActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CatalogActivity extends AppCompatActivity {
+
+
+    //Llamo a mi button desde el xml  y lo identifico por su Id
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catalog_activity);
+
+    }
 }
---
+++ sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -0,0 +1,7 @@
+package com.example.mycatalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+}
