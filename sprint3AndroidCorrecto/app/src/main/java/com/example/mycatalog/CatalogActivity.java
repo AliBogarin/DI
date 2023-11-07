4e339db90798ff25008226b98022e01fabc77a6e
Alicia Bogarin <abogarinv@fpcoruna.afundacion.org>
2023-11-07 09:06:18 +0100

    Ejercicio #3, Sprint #3, Modificado de detail_activity.xml, Completado.

--- sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,5 +1,7 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,12 +10,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CatalogActivity extends AppCompatActivity {
 
+    private Button button;
+    Context context = this;
 
     //Llamo a mi button desde el xml  y lo identifico por su Id
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalog_activity);
+
+        button = findViewById(R.id.botton_Navigate_To_Detail);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                //inicializo  DetailActivity en CatalogActivity, a traves del boton
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3AndroidCorrecto/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,7 +1,22 @@
 package com.example.mycatalog;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+
+
+    //Llamo a mi button desde el xml  y lo identifico por su Id
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+
+
+
+    }
 
 }
